6bb920607f525c2e0d6c03ef2e01d418bda13662
tahereshamimi <tahereshamimi@gmail.com>
2019-03-04 17:09:51 +0330

    add note

--- src/_components/Input.js
+++ src/_components/Input.js
@@ -1,69 +1,69 @@
-import React, { Component } from 'react';
-import { Row } from 'reactstrap';
-class Input extends Component {
-    state = {
-        note: '',
-        importance: false,
-        visibility: false,
-        importanceButton:'gray-button'
-    }
+// import React, { Component } from 'react';
+// import { Row } from 'reactstrap';
+// class Input extends Component {
+//     state = {
+//         note: '',
+//         importance: false,
+//         visibility: false,
+//         importanceButton:'gray-button'
+//     }
 
 
-    changeImportance = () => {
-        this.setState({ importance: !this.state.importance });
-        if (this.state.importance===true) {
-            this.setState({ importanceButton:'gray-button' });
-        }
-        else{
-            this.setState({ importanceButton:'red-button' });
-        }
+//     changeImportance = () => {
+//         this.setState({ importance: !this.state.importance });
+//         if (this.state.importance===true) {
+//             this.setState({ importanceButton:'gray-button' });
+//         }
+//         else{
+//             this.setState({ importanceButton:'red-button' });
+//         }
     
 
-    }
+//     }
 
-    // in this part typed note are added to state, and save buttun visibility is checked
-    addNote = (e) => {
-        if (e.target.value === '') {
-            this.setState({ visibility: false });
-        }
-        else {
-            this.setState({ visibility: true })
-        }
-        this.setState({
-            note: e.target.value
-        })
-    }
+//     // in this part typed note are added to state, and save buttun visibility is checked
+//     addNote = (e) => {
+//         if (e.target.value === '') {
+//             this.setState({ visibility: false });
+//         }
+//         else {
+//             this.setState({ visibility: true })
+//         }
+//         this.setState({
+//             note: e.target.value
+//         })
+//     }
 
-    onSubmit = (e) => {
-        if (e.target.value !== "") {
-            let note = e.target.value
-            this.setState(() => ({ note }));
-            e.target.value = "";
-        }
-        e.preventDefault();
-    }
+//     onSubmit = (e) => {
+//         if (e.target.value !== "") {
+//             let note = e.target.value
+//             this.setState(() => ({ note }));
+//             e.target.value = "";
+//         }
+//         e.preventDefault();
+//     }
 
 
-    render() {
-        return (
-            <Row className='justify-content-center' >
+//     render() {
+//         return (
+//             <Row className='justify-content-center' >
             
-                <form onSubmit={this.onSubmit} className='input-card p-3'>
-                    <div>
-                        <span className={this.state.importanceButton}
-                            onClick={this.changeImportance}>{this.state.importance ? 'urgent' : 'normal'}
-                        </span>
-                    </div>
-                    <div>
-                        <textarea className='textarea mt-3' name='note' placeholder="enter note" onChange={this.addNote} > </textarea>
-                    </div>
-                    <div>
-                        {this.state.visibility ? <span type='submit' className='save-button'>save</span> : null}
-                    </div>
-                </form>
-            </Row>
-        )
-    }
-}
+//                 <form onSubmit={this.onSubmit} className='input-card p-3'>
+//                     <div>
+//                         <span className={this.state.importanceButton}
+//                             onClick={this.changeImportance}>{this.state.importance ? 'urgent' : 'normal'}
+//                         </span>
+//                     </div>
+//                     <div>
+//                         <textarea className='textarea mt-3' name='note' placeholder="enter note" onChange={this.addNote} > </textarea>
+//                     </div>
+//                     <div>
+//                         {this.state.visibility ? <span type='submit' className='save-button'>save</span> : null}
+//                     </div>
+//                 </form>
+//             </Row>
+//         )
+//     }
+// }
 
-export { Input }
+// export { Input }
--- src/_components/Notes.js
+++ src/_components/Notes.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import { Input } from './Input';
+// import { Input } from './Input';
 
 class Notes extends Component {
     state = {
-        notes: ['first', 'second','third']
+        notes: [],
+        note: '',
+        importance: false,
+        visibility: false,
+        importanceButton:'gray-button'
+    }
+
+    changeImportance = () => {
+        this.setState({ importance: !this.state.importance });
+        if (this.state.importance===true) {
+            this.setState({ importanceButton:'gray-button' });
+        }
+        else{
+            this.setState({ importanceButton:'red-button' });
+        }
+    
+
+    }
+
+    // in this part typed note are added to state, and save buttun visibility is checked
+    addNote = (e) => {
+        if (e.target.value === '') {
+            this.setState({ visibility: false });
+        }
+        else {
+            this.setState({ visibility: true })
+        }
+        this.setState({
+            note: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        if (this.state.note !== "") {
+            let note = this.state.note
+            this.setState(() => ({ note }));
+        
+            
+        }
+        e.preventDefault();
     }
 
     addNotes = (e) => {
         console.log('tata')
-        // this.setState({ notes: prevState.push() });
-        this.setState({ note: this.state.notes.push(e.target.value) })
+        debugger
+        this.setState((prevState) => ({ 
+            notes:prevState.push(this.state.note)
+         }));
 
     }
 
@@ -27,7 +68,24 @@ class Notes extends Component {
             <div>
                 <Container>
 
-                    <Input addNotes={this.addNotes}></Input>
+                    {/* <Input addNotes={this.addNotes}></Input> */}
+                    <Row className='justify-content-center' >
+            
+                    <form onSubmit={this.onSubmit} className='input-card p-3'>
+                        <div>
+                            <span className={this.state.importanceButton}
+                                onClick={this.changeImportance}>{this.state.importance ? 'urgent' : 'normal'}
+                            </span>
+                        </div>
+                        <div>
+                            <textarea className='textarea mt-3' name='note' placeholder="enter note" onChange={this.addNote} ></textarea>
+                        </div>
+                        <div>
+                            {this.state.visibility ? <span type='submit' className='save-button' onClick={this.addNotes}>save</span> : null}
+                        </div>
+                    </form>
+                    </Row>
+
                     <Row>
                         {noteItem}
                     </Row>
