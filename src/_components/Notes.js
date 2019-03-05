118d610541b453714f0ef45b03356f631735346f
tahereshamimi <tahereshamimi@gmail.com>
2019-03-05 10:21:57 +0330

    adding notes do exactily. deleting note by clicking on them

--- src/_components/Notes.js
+++ src/_components/Notes.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-// import { Input } from './Input';
 
 class Notes extends Component {
     state = {
-        notes: [],
+        notes:[],
         note: '',
         importance: false,
         visibility: false,
-        importanceButton:'gray-button'
+        importanceButton: 'gray-button',
     }
 
     changeImportance = () => {
         this.setState({ importance: !this.state.importance });
-        if (this.state.importance===true) {
-            this.setState({ importanceButton:'gray-button' });
+        if (this.state.importance === true) {
+            this.setState({ importanceButton: 'gray-button' });
         }
-        else{
-            this.setState({ importanceButton:'red-button' });
+        else {
+            this.setState({ importanceButton: 'red-button' });
         }
-    
+
 
     }
 
@@ -34,57 +33,53 @@ class Notes extends Component {
         this.setState({
             note: e.target.value
         })
-        
+
     }
 
-    
 
     onSubmit = (e) => {
-        console.log('tata')
+        e.preventDefault();
         this.setState((prevState) => ({
-            notes:prevState.notes.push(this.state.note)
+            notes: prevState.notes.concat(this.state.note)
         }));
+        
         console.log(this.state.notes)
 
-        e.preventDefault();
-
+    }
+    deleteNote=(index)=>{
+        const notes = this.state.notes
+        notes.splice(index, 1)    
+        this.setState({notes})
     }
 
     render() {
-        const notes = this.state.notes;
-        
-        const noteItem = notes.map((note) =>
-            <Col  md='6' key={note.toString()}>
-                <div className="card-style">
-                {note}
-                </div>
-            </Col>
-        );
         return (
             <div>
                 <Container>
-
-                    {/* <Input addNotes={this.addNotes}></Input> */}
                     <Row className='justify-content-center' >
-            
-                    <form onSubmit={this.onSubmit} className='input-card p-3'>
-                        <div>
-                            <span className={this.state.importanceButton}
-                                onClick={this.changeImportance}>{this.state.importance ? 'urgent' : 'normal'}
-                            </span>
-                        </div>
-                        <div>
-                            <textarea className='textarea mt-3' name='note' placeholder="enter note" onChange={this.addNote} ></textarea>
-                        </div>
-                        <div>
-                            {/* {this.state.visibility ? <button type='submit' className='save-button' onClick={this.addNotes}>save</button> : null} */}
-                            {this.state.visibility ? <button type='submit' className='save-button'>save</button> : null}
-                        </div>
-                    </form>
+                        <form onSubmit={this.onSubmit} className='input-card p-3'>
+                            <div>
+                                <span className={this.state.importanceButton}
+                                    onClick={this.changeImportance}>{this.state.importance ? 'urgent' : 'normal'}
+                                </span>
+                            </div>
+                            <div>
+                                <textarea className='textarea mt-3' name='note' placeholder="enter note" onKeyUp={this.addNote} ></textarea>
+                            </div>
+                            <div>
+                                {this.state.visibility ? <button type='submit' className='save-button'>save</button> : null}
+                            </div>
+                        </form>
                     </Row>
 
                     <Row>
-                        {noteItem}
+                        {this.state.notes.map((note,index) => (
+                            <Col md='6'>
+                                <div key={Date.now()} className="card-style" onClick={event=>this.deleteNote(index,event)}>
+                                    {note}
+                                </div>
+                            </Col>
+                        ))}
                     </Row>
                 </Container>
 
